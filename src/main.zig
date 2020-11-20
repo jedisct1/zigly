ec4957d5635e9edcdf83b07000c3324201d71f8a
Frank Denis <github@pureftpd.org>
2020-11-20 11:22:13 +0100

    Nits

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,7 @@ const Request = zigly.Request;
 fn start() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
     defer _ = gpa.deinit();
+    var allocator = &gpa.allocator;
 
     try zigly.compatibilityCheck();
 
@@ -18,14 +19,14 @@ fn start() !void {
     var request = downstream.request;
 
     {
-        var arena = ArenaAllocator.init(&gpa.allocator);
+        var arena = ArenaAllocator.init(allocator);
         defer arena.deinit();
         const body = try request.body.readAll(&arena.allocator);
         std.debug.print("[{}]\n", .{body});
     }
 
     {
-        var arena = ArenaAllocator.init(&gpa.allocator);
+        var arena = ArenaAllocator.init(allocator);
         defer arena.deinit();
         const names = try request.headers.names(&arena.allocator);
         for (names) |name| {
@@ -34,14 +35,14 @@ fn start() !void {
     }
 
     {
-        var arena = ArenaAllocator.init(&gpa.allocator);
+        var arena = ArenaAllocator.init(allocator);
         defer arena.deinit();
         try request.headers.set(&arena.allocator, "x-test", "test");
         try request.headers.remove("x-test");
     }
 
     {
-        var arena = ArenaAllocator.init(&gpa.allocator);
+        var arena = ArenaAllocator.init(allocator);
         defer arena.deinit();
         const ua = try request.headers.get(&arena.allocator, "user-agent");
         std.debug.print("UA: [{}]\n", .{ua});
@@ -55,9 +56,9 @@ fn start() !void {
     }
 
     {
-        var arena = ArenaAllocator.init(&gpa.allocator);
+        var arena = ArenaAllocator.init(allocator);
         defer arena.deinit();
-        var query = try Request.new("GET", "http://google.com");
+        var query = try Request.new("GET", "https://google.com");
         try query.setCachingPolicy(.{ .no_cache = true });
         var response = try query.send("google.com");
         const body = try response.body.readAll(&arena.allocator);
@@ -65,7 +66,7 @@ fn start() !void {
     }
 
     {
-        var arena = ArenaAllocator.init(&gpa.allocator);
+        var arena = ArenaAllocator.init(allocator);
         defer arena.deinit();
 
         var response = downstream.response;
@@ -73,12 +74,13 @@ fn start() !void {
 
         try response.setStatus(205);
         try response.body.writeAll("OK!\n");
+        try response.finish();
     }
 
     {
-        var arena = ArenaAllocator.init(&gpa.allocator);
+        var arena = ArenaAllocator.init(allocator);
         defer arena.deinit();
-        var query = try Request.new("GET", "http://google.com");
+        var query = try Request.new("GET", "https://google.com");
         var upstream_response = try query.send("google.com");
         try downstream.response.pipe(&upstream_response, false, false);
     }
