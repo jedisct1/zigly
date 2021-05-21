24dec94a523b4cb7bd8942e35f98a9e323361dbc
Frank Denis <github@pureftpd.org>
2021-05-21 13:14:29 +0200

    Import zuri

--- src/zigly/lib.zig
+++ src/zigly/lib.zig
@@ -6,6 +6,7 @@ pub const FastlyError = errors.FastlyError;
 pub const UserAgent = @import("useragent.zig").UserAgent;
 pub const Dictionary = @import("dictionary.zig").Dictionary;
 pub const Logger = @import("logger.zig").Logger;
+pub const Uri = @import("zuri/zuri.zig").Uri;
 
 const http = @import("http.zig");
 pub const Request = http.Request;
