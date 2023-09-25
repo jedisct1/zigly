dc85bcc514da17dcb87389b961d6e59804af2660
Frank Denis <github@pureftpd.org>
2023-09-25 23:59:54 +0200

    Install libzigly

--- build.zig
+++ build.zig
@@ -4,6 +4,14 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .wasi } });
     const optimize = b.standardOptimizeOption(.{});
 
+    const lib = b.addStaticLibrary(.{
+        .name = "zigly",
+        .root_source_file = .{ .path = "src/zigly.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
+
     const exe = b.addExecutable(.{
         .name = "zig-tests",
         .root_source_file = .{ .path = "src/tests.zig" },
