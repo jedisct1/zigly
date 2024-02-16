6301ba7da6095e2784cdc5fd5cbef0a30a672689
Frank Denis <github@pureftpd.org>
2024-02-16 21:44:51 +0100

    Format

--- build.zig
+++ build.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .wasi } });
+    const target = b.standardTargetOptions(.{
+        .default_target = .{
+            .cpu_arch = .wasm32,
+            .os_tag = .wasi,
+        },
+    });
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("zigly", .{
